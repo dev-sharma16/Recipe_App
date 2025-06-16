4f09189bc580bb3d74f81842a517bde91518b5f5
Dev Sharma <devsharma1683@gmail.com>
2025-06-16 23:26:19 +0530

    added a comp for rendering rather then defining the component in this page

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,26 +1,18 @@
 import React, { useContext } from 'react'
 import { recipeContext } from '../context/RecipeContext'
+import { RecipeCard } from '../components/RecipeCard'
 
 function Recipes() {
   const {data} = useContext(recipeContext)
 
+  if(data.length <= 0) { 
+      return(<div className=" w-full p-6 bg-gray-800 text-center "><h1 className='text-3xl'>No recipes Found..!</h1></div>)
+  }
+
   return (
     <div className=" w-full p-6 bg-gray-800 flex flex-wrap justify-center items-start gap-6">
       {data.map((recipe) => (
-        <div
-          key={recipe.id}
-          className="bg-white w-72 rounded-xl shadow-lg overflow-hidden"
-        >
-          <img
-            src={recipe.image}
-            alt={recipe.title}
-            className="w-full h-40 object-cover bg-gray-200"
-          />
-          <div className="p-4">
-            <h3 className="text-lg font-semibold text-gray-800">{recipe.title}</h3>
-            <p className="text-sm text-gray-600">{recipe.description}</p>
-          </div>
-        </div>
+        <RecipeCard key = {recipe.id} recipe = {recipe}/>
       ))}
     </div>
   )
