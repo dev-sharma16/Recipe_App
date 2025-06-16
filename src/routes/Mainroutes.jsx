49bfa05b48e2666c53fd02643641aeaba5afd851
Dev Sharma <devsharma1683@gmail.com>
2025-06-16 23:28:19 +0530

    added route for the details page for a recipe

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -4,11 +4,13 @@ import Home from "../pages/Home"
 import Recipes from '../pages/Recipes'
 import About from '../pages/About'
 import Create from '../pages/Create'
+import Details from '../pages/Details'
 function Mainroutes() {
     return (
         <Routes>
             <Route path = "/" element={<Home/>}/>
             <Route path = "/recipes" element={<Recipes/>}/>
+            <Route path = "/recipes/details/:id" element={<Details/>}/>
             <Route path = "/create" element={<Create/>}/>
             <Route path = "/about" element={<About/>}/>
         </Routes>
