6c8c804e4b975638df362acb90765785498f076a
Dev Sharma <devsharma1683@gmail.com>
2025-06-19 08:54:18 +0530

    page not found Page

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div className='text-5xl mt-60 text-center text-red-400'>
+        PageNotFound
+    </div>
+  )
+}
+
+export default PageNotFound
